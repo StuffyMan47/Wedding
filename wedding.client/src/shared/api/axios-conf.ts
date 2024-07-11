3e9d72718c35dc61e9ab2fb9dcbc8fb97e8f1ccd
StuffyMan47 <fazleev_01@mail.ru>
2024-07-11 23:52:43 +0300

    fix: client

--- wedding.client/src/shared/api/axios-conf.ts
+++ wedding.client/src/shared/api/axios-conf.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5010/',
+    baseURL: 'http://185.178.46.58:5010',
 });
 
 export default instance;
--- wedding.client/vite.config.ts
+++ wedding.client/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 
+const target = `http://185.178.46.58:5010`;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,13 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
-    server: { host: false },
-    preview: { open: true },
+    server: {
+        proxy: {
+            '^/weatherforecast': {
+                target,
+                secure: false
+            }
+        },
+        port: 5173
+    }
 })
